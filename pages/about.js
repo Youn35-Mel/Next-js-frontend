9b32caacab44534adda4d8f76ea95dda8159a9e7
Younes Mel <103428027+Youn35-Mel@users.noreply.github.com>
2022-12-14 00:31:35 +0000

    first commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <div>
+      <div>yooo</div>
+      <p>find an app that helps you find dj in different events</p>
+    </div>
+  );
+};
+
+export default about;
---
+++ pages/events/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EventsPage = () => {
+  return <div>indexpage</div>;
+};
+
+export default EventsPage;
